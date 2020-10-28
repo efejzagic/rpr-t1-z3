79a1af8f93d283d1cbbbcf68c1dc1d72672ad6ce
Emil Fejzagić <efejzagic2@etf.unsa.ba>
2020-10-28 15:35:17 +0100

    Uradjen zdt 3

--- src/ba/unsa/etf/rpr/Sat.java
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -1,4 +1,67 @@
 package ba.unsa.etf.rpr;
 
 public class Sat {
+    private int sati, minute, sekunde;
+
+    public Sat(int sati, int minute, int sekunde) {
+        postavi(sati, minute, sekunde);
+    }
+
+    public int getSati() {
+        return sati;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSekunde() {
+        return sekunde;
+    }
+
+    public void postavi (int sati, int minute, int sekunde) {
+        this.sati = sati;
+        this.minute = minute;
+        this.sekunde = sekunde;
+    }
+
+    public void sljedeci() {
+        sekunde=sekunde+1;
+        if(sekunde==60) {
+            sekunde-=60; minute++;
+        }
+        if(minute==60) {
+            minute-=60; sati++;
+        }
+        if(sati==24) sati=0;
+    }
+
+    public void prethodni () {
+        sekunde--;
+        if(sekunde==-1) {
+            sekunde=59;
+            minute--;
+        }
+        if(minute==-1) {
+            minute=59;
+            sati--;
+        }
+        if(sati==-1) sati=23;
+    }
+
+    public void pomjeriZa(int pomak) {
+        if(pomak>0)
+            for(int i=0; i<pomak; i++)
+                sljedeci();
+        else
+            for(int i=0; i<-pomak; i++)
+                prethodni();
+    }
+
+    public void Ispisi() {
+        System.out.println(sati + ":" + minute + ":" + sekunde);
+    }
+
+
+
 }
